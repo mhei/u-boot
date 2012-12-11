e5891e842086cc944add63da3d9f3ab29b26e949
Michael Krummsdorf <michael.krummsdorf@tqs.de>
2012-12-11 17:12:18 +0100

    MBa28: enable usb0 power when cmd_usb
    Signed-off-by: Michael Krummsdorf <michael.krummsdorf@tqs.de>

--- board/tqc/tqma28/tqma28.c
+++ board/tqc/tqma28/tqma28.c
@@ -52,6 +52,12 @@ int board_early_init_f(void)
 	mx28_set_sspclk(MXC_SSPCLK1, 96000, 0);
 
 #ifdef	CONFIG_CMD_USB
+	/* MBa28 has two USB ports - power up both */
+	mxs_iomux_setup_pad(MX28_PAD_SSP2_SS2__USB0_OVERCURRENT);
+	mxs_iomux_setup_pad(MX28_PAD_AUART2_TX__GPIO_3_9 |
+			MXS_PAD_4MA | MXS_PAD_3V3 | MXS_PAD_NOPULL);
+	gpio_direction_output(MX28_PAD_AUART2_TX__GPIO_3_9, 1);
+
 	mxs_iomux_setup_pad(MX28_PAD_SSP2_SS1__USB1_OVERCURRENT);
 	mxs_iomux_setup_pad(MX28_PAD_AUART2_RX__GPIO_3_8 |
 			MXS_PAD_4MA | MXS_PAD_3V3 | MXS_PAD_NOPULL);
