9b9afc0a0494443551496715a721898624fe83b6
Michael Heimpold <mhe@iequalize.de>
2013-05-22 09:10:08 +0200

    Home-Box: fixup netconsole
    - Disable netconsole by default for SoM test
    - If enabled start netconsole before loading kernel et al.
    - Start netconsole unconditionally after starting linux failed
    
    Signed-off-by: Michael Heimpold <mhe@iequalize.de>

--- include/configs/homebox.h
+++ include/configs/homebox.h
@@ -227,7 +227,7 @@
 #define CONFIG_RESET_TO_RETRY			/* reset board to retry booting */
 
 #define CONFIG_BOOTFILE		"uImage"
-#define CONFIG_BOOTCOMMAND	"run resetconsole; run find_bootsys; run start_nc"
+#define CONFIG_BOOTCOMMAND	"run resetconsole; run cond_nc; run find_bootsys; run start_nc"
 #define CONFIG_LOADADDR		0x42000000
 #define CONFIG_SYS_LOAD_ADDR	CONFIG_LOADADDR
 #define CONFIG_OF_LIBFDT
@@ -286,10 +286,10 @@
 		"serialconsole=setenv stderr serial; setenv stdout serial; setenv stdin serial\0" \
 		"netconsole=setenv stderr nc; setenv stdout nc; setenv stdin nc\0"                \
 		"resetconsole=if test ${stdout} = 'nc'; then run serialconsole; saveenv; fi\0"    \
-		"netconsole_enabled=1\0"                                                          \
+		"netconsole_enabled=0\0"                                                          \
 		"ncip=192.168.9.133\0"                                                            \
-		"start_nc=if test ${netconsole_enabled} = 1; then "                               \
-			"echo starting netconsole; run netconsole; version; fi\0"                 \
+		"start_nc=echo starting netconsole; run netconsole; version\0"                    \
+		"cond_nc=if test ${netconsole_enabled} = 1; then run start_nc; fi\0"              \
 		"start_deploy="                                                                   \
 			"mw.l ${loadaddr} efbeadde 80; "                                          \
 			"mmc write ${loadaddr} 44030 1; "                                         \
